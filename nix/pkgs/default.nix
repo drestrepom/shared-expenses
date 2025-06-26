f0bd7d699852b1a5d3709f0fd9362b824d3ff768
Diego Restrepo <restrepomesadiego@gmail.com>
2025-06-26 13:22:55 -0500

    feat(infra): add ECR repository
    - build docker imagen using nix
    - push image to ECR reposiroty

--- nix/pkgs/default.nix
+++ nix/pkgs/default.nix
@@ -10,4 +10,7 @@
   front-lint = pkgs.callPackage ./front-lint { inherit lib' pkgs; };
   api-lint = pkgs.callPackage ./api-lint { inherit inputs pkgs lib'; };
   front-deploy = pkgs.callPackage ./front-deploy { inherit lib' pkgs; };
+  api-run = pkgs.callPackage ./api-run { inherit inputs pkgs lib'; };
+  api-container = pkgs.callPackage ./api-container { inherit self' pkgs; };
+  api-container-push = pkgs.callPackage ./api-container-push { inherit self' pkgs; };
 }
---
+++ shared-expenses-api/shared_expenses_api/__init__.py
@@ -0,0 +1,36 @@
+import os
+from fastapi import FastAPI, HTTPException, Depends
+import aioboto3
+from typing import AsyncGenerator
+
+# Nombre de la tabla DynamoDB
+DYNAMODB_TABLE = os.getenv("DYNAMODB_TABLE", "shared-expenses")
+
+app = FastAPI()
+
+# Dependencia para obtener el recurso de DynamoDB
+async def get_dynamodb() -> AsyncGenerator[aioboto3.Session.resource, None]:
+    session = aioboto3.Session()
+    async with session.client("dynamodb") as dynamodb:
+        yield dynamodb
+
+
+@app.get("/")
+def read_root() -> dict[str, str]:
+    return {"message": "Bienvenido al backend de gestión de gastos"}
+
+
+@app.get("/db-check")
+async def db_check(dynamodb=Depends(get_dynamodb)) -> dict[str, str]:
+    try:
+        # Realiza una operación describe_table para verificar la conexión
+        response = await dynamodb.describe_table(TableName=DYNAMODB_TABLE)
+        if response and response.get("Table", {}).get("TableStatus") == "ACTIVE":
+            return {"message": "DynamoDB connection successful!"}
+        else:
+            raise HTTPException(
+                status_code=500,
+                detail="DynamoDB connection check failed: Table not active",
+            )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"DynamoDB connection failed: {str(e)}") 
